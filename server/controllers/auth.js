5251acd4a9d15a5cbf764da0bf52f4ce867b6f11
unknown <brian.njau@strathmore.edu>
2021-08-19 17:05:52 +0300

    Adding Global State, CSRF , axios interceptors and protected routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import {hashPassword, comparePassword } from "../utils/auth";
+import jwt from 'jsonwebtoken';
 export const register = async (req, res) =>{
 try{
     // console.log(req.body);
@@ -35,4 +36,55 @@ return res.json({ok:true});
 console.log(err)
 return res.status(400).send("Error. Try Again")
 }
+}
+
+export const login = async (req, res) =>{
+    try{
+// console.log(req.body);
+//check if db has user with that email 
+const {email, password} = req.body
+const user = await User.findOne({email}).exec();
+if(!user) return res.status(400).send("No user found");
+
+//check password
+const match = await comparePassword(password, user.password);
+//create signed jwt
+const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+    
+});
+//return user and token, exclude password 
+user.password = undefined; 
+
+res.cookie("token",token,{
+    httpOnly:true,
+    // secure:true, // Only works on https
+});
+
+res.json(user);
+
+    }catch(err){
+        console.log(err)
+        return res.status(400).send("Error. Try again.");
+    }
+}
+
+export const logout = async (req , res) =>{
+    try{
+        res.clearCookie("token");
+        return res.json({message: "Signout success"})
+    }catch(err){
+        console.log(err)
+        
+    }
+}
+
+export const currentUser = async (req,res) =>{
+    try{
+        const user = await User.findById(req.user._id).select('-password').exec();
+        console.log('CURRENT USER', user);
+        return res.json({ok:true});
+    }catch(err){
+console.log(err);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import {readdirSync} from 'fs';
 import mongoose from 'mongoose'
+import csrf from 'csurf'
+import cookieParser from 'cookie-parser'
 const morgan = require("morgan");
 require("dotenv").config();
 
+const csrfProtection = csrf({cookie:true});
+
 //create express app 
 const app = express();
+
+
 
 //db
 mongoose.connect(process.env.DATABASE, {
@@ -22,10 +28,21 @@ mongoose.connect(process.env.DATABASE, {
 //apply middleware 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
+
 
 //route 
 readdirSync('./routes').map((r)=>app.use('/api', require(`./routes/${r}`)));
+
+//csrf
+app.use(csrfProtection)
+
+app.get('/api/csrf-token', (req,res)=>{
+    res.json({csrfToken:req.csrfToken()});
+});
+
+
 
 
 //port 
