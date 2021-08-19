bbe8c66b919172c62f73bd6fc0a73d9648016e8c
unknown <brian.njau@strathmore.edu>
2021-08-19 23:13:36 +0300

    Sending Emails and Password Reset

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,18 @@
 import User from '../models/user';
 import {hashPassword, comparePassword } from "../utils/auth";
 import jwt from 'jsonwebtoken';
+import AWS from 'aws-sdk'
+import {nanoid} from 'nanoid'
+
+const awsConfig = {
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+    region:process.env.AWS_REGION,
+    apiVersion:process.env.AWS_API_VERSION,
+};
+
+const SES = new AWS.SES(awsConfig);
+
 export const register = async (req, res) =>{
 try{
     // console.log(req.body);
@@ -48,6 +60,8 @@ if(!user) return res.status(400).send("No user found");
 
 //check password
 const match = await comparePassword(password, user.password);
+if(!match)return res.status(400).send("Wrong Password");
+
 //create signed jwt
 const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET, {
     expiresIn: "3d",
@@ -86,5 +100,110 @@ export const currentUser = async (req,res) =>{
         return res.json({ok:true});
     }catch(err){
 console.log(err);
+    }
+}
+
+export const sendTestEmail = async (req,res) =>{
+    // console.log("Send email using SES");
+    // res.json({ok:true});
+    const params={
+        Source:process.env.EMAIL_FROM,
+        Destination: {
+            ToAddresses:["wahinyabryan@gmail.com"],
+        },
+        ReplyToAddresses: [process.env.EMAIL_FROM],
+        Message:{
+            Body:{
+                Html:{
+                    Charset:"UTF-8",
+                    Data: `
+                    <html>
+                    <h1>Reset Password Link</h1>
+                    <p>Please use the following link to reset your password</p>
+                    </html>
+                    `,
+                },
+            },
+            Subject:{
+                Charset: "UTF-8",
+                Data: "Password reset link",
+            },
+        },
+    }
+    const emailSent = SES.sendEmail(params).promise();
+emailSent
+.then((data)=>{
+    console.log(data);
+    res.json({ok:true});
+}).catch((err)=>{
+    console.log(err);
+});
+}
+
+export const forgotPassword = async(req,res) =>{
+try{
+const {email} = req.body;
+console.log(email);
+
+const shortCode = nanoid(6).toUpperCase();
+const user = await User.findOneAndUpdate({email}, 
+    {passwordResetCode: shortCode});
+    if(!user)return res.status(400).send("User not found");
+
+    //prepare for mail 
+    const params = {
+        Source:process.env.EMAIL_FROM,
+        Destination:{
+            ToAddresses:[email]
+        },
+        Message:{
+            Body:{
+                Html:{
+                    Charset:'UTF-8',
+                    Data:`
+                    <html>
+                    <h1>Reset Password </h1>
+                    <p>Use this code to reset your password</p>
+                    <h2 style="color:red">${shortCode}</h2>
+                    <i>zerity.com</i>
+                    </html>
+                    `,
+                },
+            },
+            Subject:{
+                Charset:'UTF-8',
+                Data:"Reset Password",
+            },
+        },
+       
+    }
+    const emailSent = SES.sendEmail(params).promise();
+    emailSent.then((data)=>{
+        console.log(data);
+        res.json({ok:true});
+    }).catch((err)=>{
+        console.log(err);
+    });
+}catch(err){
+console.log(err);
+}
+
+}
+
+export const resetPassword = async(req,res)=>{
+    try{
+        const {email,code, newPassword} = req.body
+        // console.table({email,code,newPassword});
+        const hashedPassword = await hashPassword(newPassword);
+        const user = User.findOneAndUpdate({
+            email,passwordResetCode:code,
+        },{
+            password:hashedPassword,
+            passwordResetCode:"",
+        }).exec();
+        res.json({ok:true});
+    }catch(err){
+        console.log(err);
+        return res.status(400).send("Error! Try again.")
     }
 }
